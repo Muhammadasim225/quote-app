39206658294335e0ffe1fa5f05cd0f697ade21aa
Mohammad Asim <muhammadasim4927@gmail.com>
2025-05-22 19:42:18 +0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,11 +9,9 @@ const Navbar = () => {
   const token = localStorage.getItem('token');
 
   useEffect(() => {
-    // Initialize sidenav for Materialize
-    M.Sidenav.init(document.querySelectorAll('.sidenav'), {
-      edge: 'left',
-      draggable: true,
-    });
+    // Initialize Materialize sidenav
+    const elems = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(elems, { edge: 'left', draggable: true });
   }, []);
 
   const handleLogout = () => {
@@ -24,31 +22,25 @@ const Navbar = () => {
 
   return (
     <>
-      <nav className="nav-wrapper deep-purple darken-4 z-depth-3">
+      <nav className="nav-wrapper deep-purple darken-4 z-depth-2">
         <div className="container">
           <Link to="/" className="brand-logo" style={{ fontWeight: 600 }}>
             QuoteApp
           </Link>
-          <a href="#!" data-target="mobile-nav" className="sidenav-trigger">
+          <a href="#!" data-target="mobile-nav" className="sidenav-trigger right">
             <i className="material-icons">menu</i>
           </a>
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
+
+          <ul className="right hide-on-med-and-down">
             {token ? (
               <>
-                <li>
-                  <Link className="nav-link" to="/profile">
-                    Profile
-                  </Link>
-                </li>
-                <li>
-                  <Link className="nav-link" to="/create-quote">
-                    Create
-                  </Link>
-                </li>
+                <li><Link to="/profile" className="nav-link">Profile</Link></li>
+                <li><Link to="/create-quote" className="nav-link">Create</Link></li>
                 <li>
                   <button
                     onClick={handleLogout}
-                    className="btn btn-small pink lighten-1 waves-effect waves-light logout-btn"
+                    className="btn pink lighten-2 waves-effect waves-light"
+                    style={{ marginTop: "8px" }}
                   >
                     Logout
                   </button>
@@ -56,16 +48,15 @@ const Navbar = () => {
               </>
             ) : (
               <>
-                <li><Link className="nav-link" to="/login">Login</Link></li>
-                <li><Link className="nav-link" to="/signup">Signup</Link></li>
+                <li><Link to="/login" className="nav-link">Login</Link></li>
+                <li><Link to="/signup" className="nav-link">Signup</Link></li>
               </>
             )}
           </ul>
         </div>
       </nav>
 
-      {/* Mobile Nav */}
-      <ul className="sidenav deep-purple darken-3" id="mobile-nav">
+      <ul className="sidenav deep-purple darken-4" id="mobile-nav">
         {token ? (
           <>
             <li><Link className="white-text" to="/profile">Profile</Link></li>
