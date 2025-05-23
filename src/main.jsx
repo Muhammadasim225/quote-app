ed6ecf9d214893219b6ef02ce11dca44b893aed3
Mohammad Asim <muhammadasim4927@gmail.com>
2025-05-23 16:33:02 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,38 +7,44 @@ import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
-  createHttpLink,
+
 } from "@apollo/client";
-import { setContext } from '@apollo/client/link/context';
 
-// 1. Create HTTP link
-const httpLink = createHttpLink({
-  uri: import.meta.env.VITE_API_URL || "/graphql",
-});
-
-// 2. Create auth link that dynamically gets the token
-const authLink = setContext((_, { headers }) => {
-  // Get the token from localStorage every time a request is made
-  const token = localStorage.getItem("token");
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : "",
-    }
-  };
-});
-
-// 3. Create Apollo Client
 const client = new ApolloClient({
-  link: authLink.concat(httpLink), // Chain the links
+  uri: import.meta.env.VITE_API_URL || "/graphql", // Use the VITE_API_URL variable, fallback to '/graphql' in development
+
   cache: new InMemoryCache(),
-  defaultOptions: {
-    watchQuery: {
-      fetchPolicy: 'cache-and-network',
-    },
-    query: {
-      fetchPolicy: 'network-only',
-    },
+  headers:{
+    authorization:localStorage.getItem("token") || " "
+  }
+});
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </BrowserRouter>
+  </StrictMode>
+);import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: import.meta.env.VITE_API_URL || "/graphql", // Use the VITE_API_URL variable, fallback to '/graphql' in development
+
+  cache: new InMemoryCache(),
+  headers:{
+    authorization:localStorage.getItem("token") || " "
   }
 });
 
